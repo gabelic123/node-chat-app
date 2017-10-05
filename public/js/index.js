e6e6751ce2eebe7869e8437611dd481a7f23f871
GabrielLicona <gabriel@peeyrapp.com>
2017-10-04 21:05:53 -0400

    Emit new message onCreateMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'gabe',
-		text: 'Works for me'
-	});
 });
 
 socket.on('disconnect', function () {
